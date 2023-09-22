3632fc77e9a62ea4476fe1e99a0d7bc5ab601db0
mercenarioz <funnystar320@gmail.com>
2023-09-22 17:32:54 +0700

    Code base

--- src/server.js
+++ src/server.js
@@ -1,14 +1,35 @@
-import express from 'express';
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
 
-const app = express();
+import express from 'express'
+import { mapOrder } from '~/utils/sorts.js'
 
-const host = process.env.HOST || 'localhost';
+const app = express()
+
+const hostname = 'localhost'
 const port = 8000
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>')
+    // Test Absolute import mapOrder
+    console.log(
+        mapOrder(
+            [
+                { id: 'id-1', name: 'One' },
+                { id: 'id-2', name: 'Two' },
+                { id: 'id-3', name: 'Three' },
+                { id: 'id-4', name: 'Four' },
+                { id: 'id-5', name: 'Five' },
+            ],
+            ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+            'id'
+        )
+    )
+    res.send('<h1>Hello World!</h1><hr>')
 })
 
-app.listen(port, host, () => {
-    console.log(`Server is listening on ${host}:${port}`);
+app.listen(port, hostname, () => {
+    console.log(`Server is running at ${hostname}:${port}`)
 })
