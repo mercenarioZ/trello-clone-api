44c64d93b318e6bf196ea251e4f259b4ffeaac1a
mercenarioz <funnystar320@gmail.com>
2023-09-26 11:03:28 +0700

    Refactor env for project

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,36 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import { MongoClient, ServerApiVersion } from 'mongodb'
+import { env } from './environment'
+
+const MONGODB_URI = env.MONGODB_URI
+
+const DATABASE_NAME = env.DATABASE_NAME
+
+let trelloDatabaseInstance = null
+
+const mongoClientInstance = new MongoClient(MONGODB_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+})
+
+// Connect to MongoDB
+export const connectToDatabase = async () => {
+    await mongoClientInstance.connect()
+
+    trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME)
+}
+
+export const getDatabase = () => {
+    if (!trelloDatabaseInstance) {
+        throw new Error('Database not initialized')
+    }
+
+    return trelloDatabaseInstance
+}
+
+// Close MongoDB connection when exiting
+export const closeDatabase = async () => {
+    await mongoClientInstance.close()
+}
--- src/server.js
+++ src/server.js
@@ -1,35 +1,38 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
-
+import exitHook from 'async-exit-hook'
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import { closeDatabase, connectToDatabase, getDatabase } from './config/mongodb'
+import { env } from '~/config/environment'
 
-const app = express()
+const START_SERVER = () => {
+    const app = express()
 
-const hostname = 'localhost'
-const port = 8000
+    app.get('/', async (req, res) => {
+        console.log(await getDatabase().listCollections().toArray())
+        res.send('<h1>Hello World!</h1><hr>')
+    })
 
-app.get('/', (req, res) => {
-    // Test Absolute import mapOrder
-    console.log(
-        mapOrder(
-            [
-                { id: 'id-1', name: 'One' },
-                { id: 'id-2', name: 'Two' },
-                { id: 'id-3', name: 'Three' },
-                { id: 'id-4', name: 'Four' },
-                { id: 'id-5', name: 'Five' },
-            ],
-            ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-            'id'
-        )
-    )
-    res.send('<h1>Hello World!</h1><hr>')
-})
+    app.listen(env.APP_PORT, env.APP_HOST, () => {
+        console.log(`Server is running at http://${env.APP_HOST}:${env.APP_PORT}`)
+    })
 
-app.listen(port, hostname, () => {
-    console.log(`Server is running at ${hostname}:${port}`)
-})
+    // Clean up database connection when exiting
+    exitHook(() => {
+        closeDatabase()
+        console.log('Disconnected from database')
+    })
+}
+
+;(async () => {
+    try {
+        console.log('Connecting to database...')
+        // Connect to MongoDB
+        await connectToDatabase()
+        console.log('Connect to database successfully.')
+
+        // Start server
+        START_SERVER()
+    } catch (err) {
+        console.log(err)
+        process.exit(0)
+    }
+})()
